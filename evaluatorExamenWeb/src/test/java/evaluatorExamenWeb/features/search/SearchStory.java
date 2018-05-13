880b76f7d0eca670ed20addfb75b1c63c2fc5edd
Claudiu Ursuta <ursutaclaudiu@yahoo.com>
2018-05-13 13:17:59 +0300

    Added Lab 5

---
+++ evaluatorExamenWeb/src/test/java/evaluatorExamenWeb/features/search/SearchStory.java
@@ -0,0 +1,40 @@
+package evaluatorExamenWeb.features.search;
+
+import evaluatorExamenWeb.steps.serenity.EndUserSteps;
+import evaluatorExamenWeb.steps.serenity.UserSteps;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Managed;
+import net.thucydides.core.annotations.Pending;
+import net.thucydides.core.annotations.Steps;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+@RunWith(SerenityRunner.class)
+public class SearchStory
+{
+    @Managed(uniqueSession = true)
+    public WebDriver webdriver;
+
+    @Steps
+    public UserSteps anna;
+
+    @Test
+    public void searching_by_keyword_apple_should_display_the_corresponding_article() {
+        anna.is_the_home_page();
+        anna.looks_for("apple");
+        // anna.should_see_definition("A common, round fruit produced by the tree Malus domestica, cultivated in temperate climates.");
+    }
+
+//    @Test
+//    public void searching_by_keyword_banana_should_display_the_corresponding_article() {
+//        anna.is_the_home_page();
+//        anna.looks_for("pear");
+//        anna.should_see_definition("An edible fruit produced by the pear tree, similar to an apple but elongated towards the stem.");
+//    }
+//
+//    @Pending
+//    @Test
+//    public void searching_by_ambiguious_keyword_should_display_the_disambiguation_page() {
+//    }
+}
